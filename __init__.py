a6ba60518f92fbf56e97f1dbcb25573ab0f96829
Martin Brösamle <m@m-broe.de>
2016-10-24 23:30:10 +0200

    Move stripNamespace to PyBroeModules.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-import os, codecs, io, re, datetime
-from xml.etree.ElementTree import XML, XMLParser, tostring, TreeBuilder
+import os, io, codecs, re, datetime
 from string import Template
 
 try:
@@ -14,34 +13,9 @@ import xml.etree.ElementTree as ET
 import markdown
 
 import PyBroeModules.ItemsCollectionA as IC
+from PyBroeModules.StripNamespace import stripNamespace
 
 from CFG_FilesPaths import indexHTMLwww, indexHTMLlocal
-
-### Source: http://stackoverflow.com/questions/8113296/supressing-namespace-prefixes-in-elementtree-1-2
-### (I am not the only one to copy this snippet:
-### https://github.com/Remi-C/PPPP_utilities/blob/master/pointcloud/test_reading_ply_with_python.py)
-class StripNamespace(TreeBuilder):
-    """ Remove Namespace prefixes from the tags.
-        Source: http://stackoverflow.com/questions/8113296/supressing-namespace-prefixes-in-elementtree-1-2
-        """
-    def start(self, tag, attrib):
-        index = tag.find('}')
-        if index != -1:
-            tag = tag[index+1:]
-        super(StripNamespace, self).start(tag, attrib)
-    def end(self, tag):
-        index = tag.find('}')
-        if index != -1:
-            tag = tag[index+1:]
-        super(StripNamespace, self).end(tag)
-
-def stripNamespace(content):
-    target = StripNamespace()
-    parser = XMLParser(target=target)
-    root = XML(content, parser=parser)
-    s = io.StringIO()
-    ET.ElementTree(root).write(s,encoding='unicode')
-    return s.getvalue()
 
 def writeHTMLfile(outFN,documentHTML):
     print ("HTML-OUT:",outFN)
